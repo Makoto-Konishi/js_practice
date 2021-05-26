55cb1ecf2844870e8229b803118f94d49f6e1f6d
Makotox0207 <xiaoxishicheng@gmail.com>
2021-05-26 23:35:35 +0900

    mapObject

---
+++ js_test/object/mapObject.js
@@ -0,0 +1,29 @@
+const myMap = new Map();
+
+myMap.set('id', 3);
+myMap.set('name', '本田');
+
+console.log(myMap);
+
+// {"key": "id","value": 3},
+// {"key": "name","value": "本田"}
+
+console.log(myMap.get('name'));
+// => 本田
+
+const keyList = myMap.keys();
+
+for (key of keyList){
+  console.log(key);
+}
+// => id
+// => name
+
+const valueList = myMap.values();
+
+for (value of valueList){
+  console.log(value);
+}
+
+// => 3
+// => 本田
