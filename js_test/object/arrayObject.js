f5296ca0f36f3df2e6f88e0ec6ecc5c55f8d35d1
Makotox0207 <xiaoxishicheng@gmail.com>
2021-05-27 00:29:25 +0900

    Array.method "filter", "find", "map"

--- js_test/object/arrayObject.js
+++ js_test/object/arrayObject.js
@@ -17,3 +17,34 @@ console.log(fruits);
 fruits.forEach(function(input){
   console.log(input);
 });
+
+// Array.filter
+const scores = [10, 20, 30, 40];
+
+const newScores = scores.filter((value) => {
+  return value >= 30
+});
+
+console.log(newScores);
+// => [30, 40]
+
+
+// Array.find
+const members = ['本田', '香川', '長友'];
+
+const member = members.find((value) => value === '長友' );
+
+console.log(member);
+// => 長友
+
+
+// Array.map 配列を元に新しい配列をつくる
+
+const userList = [10, 20, 30, 40];
+
+const userIdList = userList.map((value) => {
+  return `user_${value}`;
+});
+
+console.log(userIdList);
+// => ["user_10", "user_20", "user_30", "user_40"]
