5a2ed3020fce77d9c658f1cfae21581721873ae5
Makotox0207 <xiaoxishicheng@gmail.com>
2021-05-27 23:28:00 +0900

    getElement

---
+++ js_test/DOM/getElement.js
@@ -0,0 +1,13 @@
+const test = document.getElementById('test');
+console.log(test);
+
+
+
+const testList = document.querySelector('.test_list');
+console.log(testList);
+// => <div id="test" class="test_list"> 
+
+const testLists = document.querySelectorAll('.test_list');
+console.log(testLists);
+
+//=>  NodeList [div#test.test_list]
